29c9a33ed27d40e6b29069de1451d1fea96be131
Bryan Harris <bryanlharris@me.com>
2025-06-26 18:02:31 -0400

    Databricks not letting me delete files

--- layer_02_silver/edsm_silver_pipeline.py
+++
@@ -1,8 +0,0 @@
-
-
-def edsm_silver_pipeline(spark, settings):
-    df = edsm_silver_read(spark, settings)
-    df = edsm_silver_transform(spark, settings, df)
-    query = edsm_silver_write(spark, settings, df)
-
-
--- layer_03_gold/edsm_gold_read.py
+++
@@ -1,5 +0,0 @@
-
-
-def edsm_gold_read(spark, settings):
-    return spark.read.table(settings["src_table_name"])
-
