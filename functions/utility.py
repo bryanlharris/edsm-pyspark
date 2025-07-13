e99cd655151d48eb8340d2360a543a949a681654
Bryan Harris <bryanlharris@me.com>
2025-07-12 20:27:47 -0400

    update delta version

--- functions/utility.py
+++ functions/utility.py
@@ -18,7 +18,7 @@ def create_spark_session(master: str, app_name: str):
     return (
         SparkSession.builder.master(master)
         .appName(app_name)
-        .config("spark.jars.packages", "io.delta:delta-spark_2.12:2.4.0")
+        .config("spark.jars.packages", "io.delta:delta-spark_2.12:3.3.2")
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config(
             "spark.sql.catalog.spark_catalog",
